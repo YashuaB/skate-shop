bb77b3da03d010e6549a6f8413704256854aceca
monikanikolova <monika.nikolova@yahoo.com>
2019-06-12 22:56:54 -0400

    folders for pages and components created

--- skate-shoppe-app/src/App.js
+++ skate-shoppe-app/src/App.js
@@ -1,34 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Nav from "./components/Nav";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Collections from "./pages/Collections";
+import Home from "./pages/Home";
+import LogIn from "./pages/LogIn";
 
-import React, { Component } from 'react';
-class counter extends Component {
-  render() { 
-    return ( <h1>Hello World!</h1> );
-  }
-}
- 
-export default counter;
+
+
+
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/collections" component={Collections} />
+          <Route exact path="/login" component={LogIn} />
+          {/* <Route component={NoMatch} /> */}
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ skate-shoppe-app/src/components/Nav/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./style.css";
+
+
+function Nav() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+      <a className="navbar-brand" href="/">
+      Skate Shoppe
+      </a>
+
+    
+    </nav>
+  );
+}
+
+export default Nav;
