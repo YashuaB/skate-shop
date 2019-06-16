f751d9003727d3c0f3b526b739cab74d4b562ff0
monikanikolova <monika.nikolova@yahoo.com>
2019-06-15 22:30:32 -0400

    shoppingCart component added

---
+++ skate-shoppe-app/src/pages/ShoppingCart.js
@@ -0,0 +1,118 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import {
+  removeItem,
+  addQuantity,
+  subtractQuantity
+} from "./actions/shoppingActions";
+import Recipe from "./Recipe";
+
+class ShoppingCart extends Component {
+  // Remove an item from shopping cart
+  handleRemove = id => {
+    this.props.removeItem(id);
+  };
+
+  //Add to the ordered quantity
+  handleAddQuantity = id => {
+    this.props.addQuantity(id);
+  };
+
+  //Substract from the ordered quantity
+  handleSubtractQuantity = id => {
+    this.props.subtractQuantity(id);
+  };
+
+  render() {
+    //If there are any added items, display them in cards
+    let addedItems = this.props.items.length ? (
+      this.props.items.map(item => {
+        return (
+          <li className="collection-item avatar" key={item.id}>
+            <div className="item-img">
+              <img src={item.img} alt={item.img} className="" />
+            </div>
+
+            <div className="item-desc">
+              <span className="title">{item.title}</span>
+              <p>{item.desc}</p>
+              <p>
+                <b>Price: {item.price}$</b>
+              </p>
+              <p>
+                <b>Quantity: {item.quantity}</b>
+              </p>
+              <div className="add-remove">
+                <Link to="/cart">
+                  <i
+                    className="material-icons"
+                    onClick={() => {
+                      this.handleAddQuantity(item.id);
+                    }}
+                  >
+                    arrow_drop_up
+                  </i>
+                </Link>
+                <Link to="/cart">
+                  <i
+                    className="material-icons"
+                    onClick={() => {
+                      this.handleSubtractQuantity(item.id);
+                    }}
+                  >
+                    arrow_drop_down
+                  </i>
+                </Link>
+              </div>
+              <button
+                className="waves-effect waves-light btn pink remove"
+                onClick={() => {
+                  this.handleRemove(item.id);
+                }}
+              >
+                Remove
+              </button>
+            </div>
+          </li>
+        );
+      })
+    ) : (
+      //If there are no items added, show this message
+      <p>Your shopping cart is empty!</p>
+    );
+    // Display all cards with added items here
+    return (
+      <div className="container">
+        <div className="cart">
+          <h5>You have ordered:</h5>
+          <ul className="collection">{addedItems}</ul>
+        </div>
+        <Recipe />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    items: state.addedItems
+  };
+};
+const mapDispatchToProps = dispatch => {
+  return {
+    removeItem: id => {
+      dispatch(removeItem(id));
+    },
+    addQuantity: id => {
+      dispatch(addQuantity(id));
+    },
+    subtractQuantity: id => {
+      dispatch(subtractQuantity(id));
+    }
+  };
+};
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ShoppingCart);
