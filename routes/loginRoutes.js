e3209e092cd1d39419bc5f5146a69f98b0afe746
yashua <yashuab.abraham@gmail.com>
2019-06-24 01:49:26 -0400

    fixed post for registration page

--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -5,18 +5,18 @@ module.exports = function(app){
 
 
   app.post("/register", function(req,res){
-    
+    console.log(req.body)
     db.User.create({
-      username: req.body.username,
-       email: req.body.email,
-       password: req.body.password
+      username: req.body.user.username,
+       email: req.body.user.email,
+       password: req.body.user.password
     })
       .then(function(dbUser) {
         res.json(dbUser);
       })
       .catch(function(err, res){
         if (err){
-          console.log(err)
+          console.log(err,)
         }else
         {res.send("done")}
       })
