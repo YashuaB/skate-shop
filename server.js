601a9b31f93fcfc02eee562469cd7c57c1728096
yashua <yashuab.abraham@gmail.com>
2019-07-01 23:49:41 -0400

    test 3

--- server.js
+++ server.js
@@ -61,6 +61,9 @@ app.use(passport.session()) // calls the deserializeUser
 
 if (process.env.NODE_ENV === "production"){
 	app.use(express.static("client/build"))
+	app.get("*", (req,res) =>{
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+	})
 }
 // Routes
 require('./routes/loginRoutes')(app)
